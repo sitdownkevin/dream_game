42c36589cdc44ec23c223e8e662a0c5c27fb44d8
Ke Xu <kexu567@gmail.com>
2025-05-24 15:56:31 +0800

    Update code

--- workflow.py
+++ workflow.py
@@ -7,8 +7,10 @@ from llm.role.SoulLLM import SoulLLM
 from llm.scene.SituationALLM import SituationALLM
 from llm.scene.SituationAOptLLM import SituationAOptLLM
 
-import sys, os
+import sys
+import os
 import asyncio
+
 
 class Workflow:
     def __init__(self, verbose: bool = False):
@@ -20,10 +22,10 @@ class Workflow:
         self.soul_llm = SoulLLM()
         self.situation_llm = SituationALLM()
         self.situation_aopt_llm = SituationAOptLLM()
-        
+
         self.verbose = verbose
         self.data = None
-        
+
     async def generate_information(self):
         print("Generating information...")
         tasks = [
@@ -34,72 +36,68 @@ class Workflow:
         if self.verbose:
             print(soul)
             print(theme)
-            
+
         print("Generating background...")
         background = await self.background_llm.arun(theme=theme)
         if self.verbose:
             print(background)
-            
+
         print("Generating character...")
         character = await self.charac_llm.arun(theme=theme, background=background)
         if self.verbose:
             print(character)
-            
+
         print("Generating dream...")
         dream_true = await self.dream_llm.arun(type='true', theme=theme, background=background, character=character)
         if self.verbose:
             print(dream_true)
-            
+
         print("Generating dream fake...")
         dream_fake = await self.dream_llm.arun(type='fake', theme=theme, background=background, character=character, dream_true=dream_true)
         if self.verbose:
             print(dream_fake)
-            
+
         print("Generating condition...")
         tasks = [
-            self.condition_llm.arun(type='true', theme=theme, background=background, character=character, dream=dream_true),
-            self.condition_llm.arun(type='fake', theme=theme, background=background, character=character, dream=dream_fake),
+            self.condition_llm.arun(
+                type='true', theme=theme, background=background, character=character, dream=dream_true),
+            self.condition_llm.arun(
+                type='fake', theme=theme, background=background, character=character, dream=dream_fake),
         ]
         condition_true, condition_fake = await asyncio.gather(*tasks)
         if self.verbose:
             print(condition_true)
             print(condition_fake)
-            
+
         print("Generating situation...")
-        tasks = [
-            self.situation_llm.arun(
-                theme=theme['theme'],
-                soul=soul['soul'],
-                background=background['background'],
-                character=character,
-                dream_true=dream_true['dream'],
-                dream_fake=dream_fake['dream'],
-                condition_true=condition_true['condition'],
-                condition_fake=condition_fake['condition'],
-            ),
-        ]
-        situation = await asyncio.gather(*tasks)
+        situation = await self.situation_llm.arun(
+            theme=theme['theme'],
+            soul=soul['soul'],
+            background=background['background'],
+            character=character,
+            dream_true=dream_true['dream'],
+            dream_fake=dream_fake['dream'],
+            condition_true=condition_true['condition'],
+            condition_fake=condition_fake['condition'],
+        )
         if self.verbose:
             print(situation)
-            
+
         print("Generating situation options...")
-        tasks = [
-            self.situation_aopt_llm.arun(
-                soul=soul['soul'],
-                theme=theme['theme'],
-                background=background['background'],
-                character=character,
-                dream_true=dream_true['dream'],
-                dream_fake=dream_fake['dream'],
-                condition_true=condition_true['condition'],
-                condition_fake=condition_fake['condition'],
-                current_situation_description=situation['description'],
-            ),
-        ]
-        situation_options = await asyncio.gather(*tasks)
+        situation_options = await self.situation_aopt_llm.arun(
+            soul=soul['soul'],
+            theme=theme['theme'],
+            background=background['background'],
+            character=character,
+            dream_true=dream_true['dream'],
+            dream_fake=dream_fake['dream'],
+            condition_true=condition_true['condition'],
+            condition_fake=condition_fake['condition'],
+            current_situation_description=situation['description'],
+        )
         if self.verbose:
             print(situation_options)
-        
+
         self.data = {
             'soul': soul,
             'theme': theme,
